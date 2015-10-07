64d90bcadb73fcf9ad87e126ac8b98cba5d31a53
root <root@ip-10-20-11-153.ec2.internal>
2015-10-07 19:38:14 +0000

    added sudo check

--- agent/agent.py
+++ agent/agent.py
@@ -2,17 +2,21 @@
 #import pycurl
 
 
+def is_sudo(user_name):
+    return user_name in open('/etc/sudoers','r').read()
+
+
 group_assoc = {}
 with open('/etc/group','r') as group_file:
     for group in group_file:
-        import code; code.interact(local=locals())
         group_data =  group.split(":")
         group_assoc[group_data[2]] = group_data[0]
+
+
 user_dict = {}
 with open('/etc/passwd','r') as user_file:
     for user in user_file:
-        import code; code.interact(local=locals())
         user_data = user.split(":")
-        user_dict[user[0]] = { "group": group_assoc[user_data[3]], "sudo": False}
+        user_dict[user_data[0]] = { "group": group_assoc[user_data[3]], "sudo": is_sudo(user_data[0])}
 
 print user_dict
